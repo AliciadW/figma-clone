d16e0bd4171d2d6e528beb13a6c491c322661c88
aliciadewet <alicia.dewet@gmail.com>
2025-04-16 13:43:17 +0100

    :passport_control: create user on signup

--- src/app/actions/auth.ts
+++ src/app/actions/auth.ts
@@ -2,6 +2,9 @@
 
 import { signupSchema } from "~/schemas";
 import { ZodError } from "zod";
+import { db } from "~/server/db";
+import bcrypt from "bcryptjs";
+import { redirect } from "next/navigation";
 
 export async function register(
   prevState: string | undefined,
@@ -12,9 +15,30 @@ export async function register(
       email: formData.get("email"),
       password: formData.get("password"),
     });
+
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (user) {
+      return "User already exists.";
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await db.user.create({
+      data: {
+        email,
+        password: hash,
+      },
+    });
   } catch (error) {
     if (error instanceof ZodError) {
       return error.errors.map((error) => error.message).join(" ");
     }
   }
+
+  redirect("/signin");
 }
